5a52b8529315523e46f2118dc448665dc59476dc
shidul <shaidulazam@gmail.com>
2024-02-17 13:03:17 -0500

    First Webdriver

---
+++ src/main/java/webdriver_basics/Selenium_webdriver_copy/App.java
@@ -0,0 +1,13 @@
+package webdriver_basics.Selenium_webdriver_copy;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/webdriver_basics/Selenium_webdriver_copy/AppTest.java
@@ -0,0 +1,17 @@
+package webdriver_basics.Selenium_webdriver_copy;
+
+import static org.junit.Assert.assertTrue;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    public static void main(String[] args)
+    {
+    	WebDriver driver=new ChromeDriver();
+    }
+}
